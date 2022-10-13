7a77c80c3cdc687b113592c3d0cd1b9e1de50481
Sam Urmy <oceanographerschoice@gmail.com>
2022-10-13 14:27:52 -0700

    Refactor code structure
    - Get rid of "problem"s
    - Define "apes" function to run on whole datasets
    - Check for data numerical precision and increase if needed

--- src/ProbabilisticEchoInversion.jl
+++ src/ProbabilisticEchoInversion.jl
@@ -3,70 +3,113 @@ module ProbabilisticEchoInversion
 using DimensionalData, DimensionalData.Dimensions
 using Turing
 using Statistics
+using DoubleFloats
+using ProgressMeter
+import Logging
 
 export F,
-    StaticAPESProblem,
-    StaticAPESModel,
+    AbstractSolver,
     MCMCSolver,
-    solve
+    solve,
+    iterspectra,
+    mapspectra,
+    check_precision,
+    apes
 
 @dim F YDim "Frequency (kHz)"
 
+abstract type AbstractSolver end
 
-# echomodel(sv, nprior, metadata, params)
+Base.@kwdef struct MCMCSolver <: AbstractSolver
+    sampler = NUTS(0.8)
+    nsamples = 1000
+    args = ()
+    kwargs = ()
+    verbose=false
+end
 
+"""
+    solve(data, model, solver[, params])
 
-@model function StaticAPESModel(sv, TS, logn_prior, cv_prior, sv_inflated, sv_inflation)
-    nfreqs = size(TS, 1)
-    Σbs = exp10.(TS ./ 10)
-    cv ~ cv_prior
-    ηsv = max.(cv .* sv, sqrt(eps()) * sv_inflation)
-    logn ~ arraydist(logn_prior)
-    n = exp.(logn)
-    sv_pred = Σbs * n * sv_inflation
-
-    for i in 1:nfreqs
-        if ! ismissing(sv_inflated)
-            sv_inflated[i] ~ Normal(sv_pred[i], ηsv[i])
-        end
+Run the probabilistic inverse model defined by `model` on the acoustic backscatter
+spectrum in `data`, using `solver` as the inference engine.
+"""
+function solve(data, model::Function, solver::MCMCSolver, params=())
+    m = model(data, params)
+    if solver.verbose
+        return sample(m, solver.sampler, solver.nsamples, solver.args...; solver.kwargs...)
     end
+    logger = Logging.SimpleLogger(Logging.Error)
+    chain = Logging.with_logger(logger) do
+        sample(m, solver.sampler, solver.nsamples, solver.args...; solver.kwargs...)
+    end
+    return chain
 end
 
-struct StaticAPESProblem
-    sv
-    sv_inflation
-    sv_inflated
-    TS
-    n_prior
-    cv_prior
-    model
+function iterspectra(echogram, freqdim=:F)
+    freqs = collect(dims(echogram, freqdim))
+    dd = otherdims(echogram, freqdim)
+    pointskeys = zip(DimPoints(dd), DimKeys(dd))
+    itr = ((coords=tup[1], freqs=freqs, backscatter=@view echogram[tup[2]...]) for tup in pointskeys)
+    return itr
 end
 
-function StaticAPESProblem(sv, TS, n_prior, cv_prior)
-    sv_inflation = all(ismissing.(sv)) ? 1.0 : 1/maximum(skipmissing(sv))
-    sv_inflated = sv .* sv_inflation
-    model = StaticAPESModel(sv, TS, n_prior, cv_prior, sv_inflated, sv_inflation)
-    return StaticAPESProblem(
-        sv, 
-        sv_inflation, 
-        sv_inflated, 
-        TS, 
-        n_prior, 
-        cv_prior, 
-        model)
+function mapspectra(f, echogram, freqdim=:F)
+    itr = iterspectra(echogram, freqdim)
+    result = @showprogress map(f, itr)
+    dd = otherdims(echogram, freqdim)
+    return DimArray(result, dd)
 end
 
-struct MCMCSolver
-    sampler
-    nsamples
-    args
-    kwargs
-    summarizer
+function check_precision(data, t=Double64)
+    if any(abs.(data.backscatter) .< sqrt(eps(eltype(data.backscatter))))
+        bacscatter = t.(data.backscatter)
+    else
+        backscatter = data.backscatter
+    end
+    return (coords = data.coords, freqs=data.freqs, backscatter)
 end
-MCMCSolver() = MCMCSolver(NUTS(0.8), 1000, (), (), x -> x)
 
-function solve(prob::StaticAPESProblem, solver::MCMCSolver)
-    return sample(prob.model, solver.sampler, solver.nsamples, solver.args..., solver.kwargs...)
+
+"""
+    apes(echogram, model, solver[; params, result_handler, safe_precision])
+
+Run the automatic probabilistic echo solver defined by the inverse `model` and 
+solution method `solver` on the acoustic backstter data in `echogram`.
+
+# Arguments
+- `echogram::DimArray`: Acoustic backscatter data in the linear domain. The 
+    last dimension of the `DimArray` should be named `:F` and index the
+    acoustic frequencies; all other dimensions should reference spatial/temporal
+    coordinates.
+- `model::Function`: Probabilistic inverse model defined with Turing.jl
+    or DynamicPPL.jl. This model should have the signature `model(data, params)`,
+    where `data` and `params` are `NamedTuple`s containing acoustic data and any
+    additional parameters. See below for more details.
+- `solver::AbstractSolver`: The method used to solve the inverse problem
+    specified in `model`. See `MCMCSolver` and `MAPSolver` for more detail.
+- `params`: Optional additional params to pass to `model`.
+- `result_handler`: Optional function to transform the output of the solver 
+    before (for instance, by calculating the means of a Markov chain).
+- `safe_precision::Bool=true`: Check whether backscatter absolute values are small
+    enough to introduce floating-point errors. If so, convert them to higher-
+    precision representation (default is `Double64`). Setting this to false may 
+    speed up inference, but 
+
+# Details
+
+
+"""
+function apes(echogram::DimArray, model::Function, solver::AbstractSolver; 
+        params=(), result_handler=(x)->x, safe_precision=true)
+    # numerical precision check here?
+    # parallel options?
+    function f(x)
+        x = safe_precision ? check_precision(x) : x
+        res = solve(x, model, solver, params)
+        return result_handler(res)
+    end
+    return mapspectra(f, echogram)
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using Test
 using ProbabilisticEchoInversion
 using DimensionalData
+using DoubleFloats
 using Statistics
 using Turing
 
@@ -12,11 +13,35 @@ function mappointsslices(f, a, dims)
     return DimArray(result, dd)
 end
 
-f(coords, sv) = mean(sv)
 
-post = mappointsslices(f, echogram, :F)
+iterspectra(echogram)
+
+f(data) = data.freqs' * data.backscatter / sum(data.backscatter)
+map(f, iterspectra(echogram))
+mapspectra(f, echogram)
 
 
 TS = randn(size(echogram, :F), 4)
-prob = StaticAPESProblem(echogram[1, 1, :], TS, fill(Normal(), 4), Exponential(0.1))
-sol = solve(prob, MCMCSolver())
+
+@model function examplemodel(data, params)
+    nspp = size(params.TS, 2)
+    Σ = exp10.(params.TS / 10)
+    logn ~ filldist(Normal(0, 1), nspp)
+    n = exp.(logn)
+    η ~ Exponential(0.1)
+    ϵ = data.backscatter .* η
+    data.backscatter .~ Normal.(Σ * n, ϵ)
+end
+
+par = (TS = TS,)
+data = first(iterspectra(echogram))
+mod = examplemodel(data, par)
+solver = MCMCSolver(nsamples=500, kwargs=(progress=false,))
+
+solve(data, examplemodel, MCMCSolver(), par)
+solve(data, examplemodel, solver, par)
+
+result = apes(echogram, examplemodel, solver, params=par, result_handler=mean)
+result = apes(echogram, examplemodel, solver, params=par, result_handler=mean, safe_precision=false)
+@test size(result, 1) == size(echogram, 1)
+@test size(result, 2) == size(echogram, 2)
